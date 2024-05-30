c783328644c632147e289c181202b9c042551140
NaaAbbey <dnyabbey@st.ug.edu.gh>
2024-05-30 16:45:16 +0000

    Added ongoing tasks usind flat list

--- UIMockUp/App.js
+++ UIMockUp/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, Image, TextInput, } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, TextInput, FlatList, } from 'react-native';
 import CustomButton from './components/CustomButton';
 import CategoryCard from './components/CategoryCard';
 
@@ -41,6 +41,23 @@ export default function App() {
           <CategoryCard imageSource ={require('./assets/3d-business-little-girl-reading-a-book.png')} header={'Read Books'} tasks={'12 tasks'} />
         </ScrollView>
       </View>
+
+      <View>
+        <Text style={styles.subheader}>Ongoing tasks</Text>
+        <FlatList 
+          data={[
+                  { key : "1", text : "Mobile App Development" }, { key : "2", text : "Web Development" },
+                  { key : "3", text : "Push Ups" }, { key : "4", text : "Planks" },
+                  { key : "5", text : "Preparation For Quiz" }, { key : "6", text : "BreakFast" },
+                  { key : "7", text : "Reading" }, { key : "8", text : "Chores" },
+                  { key : "9", text : "UI Design" }, { key : "10", text : "Weight Lifting" }, 
+                  { key : "11", text : "Math Assignment" },{ key : "12", text : "Preparation For Exams" }, { key : "13", text : "BreakFast" },
+                  { key : "14", text : "Meeting At 10:30 AM" }, { key : "15", text : "Laundry" },
+              ]}
+          renderItem={ ({item}) => <Text style={styles.tasks}>{ item.text }</Text> }
+        />
+      </View>
+
     </ScrollView>
   );
 }
@@ -49,6 +66,7 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#F7F0E8',
     fontFamily: 'lato',
+    paddingBottom: '15px',
   },
   intro:{
     display: 'flex',
@@ -84,7 +102,7 @@ const styles = StyleSheet.create({
   searchbar:{
     border: 'solid 1px  #FBF9F7',
     borderRadius: '14px',
-    width: '100%',
+    width: '280px',
     height: '49px',
     paddingLeft: '50px',
     marginLeft: '18px',
@@ -106,5 +124,18 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     marginLeft: '23px',
     marginTop: '42px',
+  },
+  tasks:{
+    height: '128px',
+    width: '90%',
+    border: 'solid 1px #E8D1BA',
+    backgroundColor: '#FBF9F7',
+    borderRadius: '15px',
+    fontWeight: 700,
+    marginLeft: '20px',
+    alignContent: 'center',
+    paddingLeft: '20px',
+    marginTop: '20px',
+    fontSize: '16px',
   },
 });
